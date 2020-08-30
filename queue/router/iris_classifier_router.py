0bae8a26f6d88a48b371736583dee5bfbb67c35c
Valenti Erra <valentierr@tuta.io>
2020-08-29 20:46:58 -0500

    Changing router prefix

--- queue/router/iris_classifier_router.py
+++ queue/router/iris_classifier_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from queue.iris_classifier import IrisClassifier
+from iris.iris_classifier import IrisClassifier
 from starlette.responses import JSONResponse
 
 router = APIRouter()
